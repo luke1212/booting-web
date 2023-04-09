361000e8379ab442f03bae6822414860765ec322
Luke Sun <luke890602@gmail.com>
2023-04-08 22:17:36 -0500

    first commit

---
+++ src/main/java/com/luke/sping/boot/bootingweb/BootingWebApplication.java
@@ -0,0 +1,13 @@
+package com.luke.sping.boot.bootingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootingWebApplication.class, args);
+	}
+
+}
